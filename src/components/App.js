e431eb7b5183a6b76432155a2c2a0185cf616ec2
ARCHXTEKK <slaughterer_archxtekk@mail.ru>
2023-06-03 12:16:14 +0500

    added styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 class App extends  React.Component {
   constructor(props) {
-    super(props); 
+    super(props);
+    this.state = {
+      temperature: 0,
+    } 
   }
+
   render() {
     return (
-      <div> 
-
+      <div className='container'> 
+        <div className='temperature'>
+          <div className='temperature__content'>{this.state.temperature}°C</div>
+        </div>
+        <button className='btn' id='btn-increment'>+</button>
+        <button className='btn' id='btn-decrement'>-</button>
       </div>
     );
   }
